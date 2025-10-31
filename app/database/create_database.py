4e0a89c6f886c07a9149a7e16eef3de96815be8b
noblearya <shukla.ar@northeastern.edu>
2025-10-31 13:26:47 -0400

    reworded

--- app/database/create_database.py
+++ app/database/create_database.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Load .env
+# Load environment variables
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app/database/bank.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./bank.db")
 
 # Engine and session
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
